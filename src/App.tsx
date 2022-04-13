99c1539b9d61aaba258a43358ff2949c47db688e
Christian Fassy <christianfassy@gmail.com>
2022-04-13 10:41:19 -0400

    Project Setup Fix

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+        <nav><img src="KUBU.png" className="bandCat"></img></nav>
+
+        <div className="topBox">
+
+          <nav className="homeText">
+            <h1 className="center">BandCat Instruments - Home</h1>
+          </nav>
+          <nav><img src="KUBU.png" className="bandCat"></img></nav>
+
+        </div>
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
