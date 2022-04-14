4e58132864821b5732e4cfbb0e2b1175c8341a1f
Ginja22 <88261803+Ginja22@users.noreply.github.com>
2022-04-13 22:04:03 -0500

    Added new instruments
    Added 4 more instruments/organized in Alphabetical order.

--- src/components/model/InstrumentOptions.ts
+++ src/components/model/InstrumentOptions.ts
@@ -1,11 +1,14 @@
-enum InstrumentOptions
-{
-    NONE = "NONE",
-    GUITAR = "GUITAR",
-    PIANO = "PIANO",
-    SAXOPHONE = "SAXOPHONE",
-    TRUMPET = "TRUMPET",
-    CLARINET = "CLARINET"
+enum InstrumentOptions {
+  NONE = "NONE",
+  BASS = "BASS",
+  CLARINET = "CLARINET",
+  DRUMS = "DRUMS",
+  GUITAR = "GUITAR",
+  HARMONICA = "HARMONICA",
+  PIANO = "PIANO",
+  SAXOPHONE = "SAXOPHONE",
+  SINGER = "SINGER",
+  TRUMPET = "TRUMPET",
 }
 
 export default InstrumentOptions;
