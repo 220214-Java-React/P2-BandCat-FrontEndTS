5ec4e1a1ad3dce70407753a3e6ae45559091451c
Marcus Glover <marcusg73@yahoo.com>
2022-04-15 20:53:25 -0400

    Simplified Sign Up code

--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -4,57 +4,44 @@ import Instrument from "./model/Instrument";
 import InstrumentOptions from "./model/InstrumentOptions";
 import User from "./model/User";
 import { Link, Navigate } from "react-router-dom";
+import { baseURL } from "./URL";
 
 export default function SignUp() {
-  // State  Variables to track info from Form
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [instrument, setInstrument] = useState(InstrumentOptions.GUITAR);
-  const [confidence, setConfidence] = useState(1);
 
   // Success of Sign Up
   const [signUpSuccess, setSignUpSuccess] = useState(false);
+  const [inputFields, setInputFields] = useState(
+    {
+      username: '',
+      password: '',
+      email: '',
+      phoneNumber: '',
+      instrumentName: InstrumentOptions.BASS,
+      confidence: 1
+    }
+  );
 
-  // Mutator methods to adjust state variables
-  function changeUsername(e: any) {
-    setUsername(e.target.value);
-  }
-
-  function changePassword(e: any) {
-    setPassword(e.target.value);
-  }
-
-  function changeEmail(e: any) {
-    setEmail(e.target.value);
-  }
-
-  function changePhoneNumber(e: any) {
-    setPhoneNumber(e.target.value);
-  }
-
-  function changeInstrument(e: any) {
-    setInstrument(e.target.value);
-  }
-
-  function changeConfidence(e: any) {
-    setConfidence(e.target.value);
+  // Gets User input for each field based on its element name
+  function getInput(event: any) {
+    setInputFields({
+      ...inputFields,
+      [event.target.name]: event.target.value
+    });
   }
 
   // When Sign Up Button is Pressed
   async function sendInfo() {
     // Initialize User object with typed information
     let userToCreate: User = {
-      username: username,
-      password: password,
-      email: email,
-      phoneNumber: phoneNumber,
+      username: inputFields.username,
+      password: inputFields.password,
+      email: inputFields.email,
+      phoneNumber: inputFields.phoneNumber
     };
 
     // POST request to API with user info, set headers to JSON, and retrieve the User back
-    let userReturned = await axios
-      .post("http://localhost:8080/users", JSON.stringify(userToCreate), {
+    let userReturned = await baseURL
+      .post("/users", JSON.stringify(userToCreate), {
         headers: {
           "Content-Type": "application/json;charset=UTF-8",
         },
@@ -66,14 +53,14 @@ export default function SignUp() {
     // Initialize Instrument object with selected information
     let instrumentForUser: Instrument = {
       instrumentID: userReturned.userID, // Assign ID for DB mapping
-      instrumentName: instrument,
-      confidence: confidence,
+      instrumentName: inputFields.instrumentName,
+      confidence: inputFields.confidence
     };
 
     // POST request to API with instrument info, set JSON header
-    await axios
+    await baseURL
       .post(
-        "http://localhost:8080/instruments/" + instrumentForUser.instrumentID,
+        "/instruments/" + instrumentForUser.instrumentID,
         JSON.stringify(instrumentForUser),
         {
           headers: {
@@ -106,13 +93,10 @@ export default function SignUp() {
           <div className="topBox">
 
             <div className="homeText">BandCat Instruments - Sign Up</div>
+
           </div>
         </div>
-        {/* <div className="topBox">
-            
-            <div className="homeText">BandCat Instruments - Login</div>
-          </div>
-     */}
+
         <div className="historyBox" >
           <div className="bodyText">
             <form className="center" style={{ margin: '25px 0px' }}>
@@ -126,8 +110,8 @@ export default function SignUp() {
                 type="text"
                 placeholder="Username"
                 name="username"
-                value={username}
-                onChange={changeUsername}
+                value={inputFields.username}
+                onChange={getInput}
               />
               <br /><br />
 
@@ -139,8 +123,8 @@ export default function SignUp() {
                 type="password"
                 placeholder="Password"
                 name="password"
-                value={password}
-                onChange={changePassword}
+                value={inputFields.password}
+                onChange={getInput}
               />
               <br /><br />
 
@@ -152,8 +136,8 @@ export default function SignUp() {
                 type="text"
                 placeholder="Email"
                 name="email"
-                value={email}
-                onChange={changeEmail}
+                value={inputFields.email}
+                onChange={getInput}
               />
               <br /><br />
 
@@ -165,8 +149,8 @@ export default function SignUp() {
                 type="text"
                 placeholder="Phone Number"
                 name="phoneNumber"
-                value={phoneNumber}
-                onChange={changePhoneNumber}
+                value={inputFields.phoneNumber}
+                onChange={getInput}
               />
               <br /><br />
 
@@ -176,8 +160,9 @@ export default function SignUp() {
               </label>
               <select
                 id="instrument_name"
-                value={instrument}
-                onChange={changeInstrument}
+                name="instrumentName"
+                value={inputFields.instrumentName}
+                onChange={getInput}
               >
                 <option value={InstrumentOptions.BASS}>Bass</option>
                 <option value={InstrumentOptions.CLARINET}>Clarinet</option>
@@ -195,7 +180,11 @@ export default function SignUp() {
               <label>
                 Confidence:<span> </span>
               </label>
-              <select id="confidence" value={confidence} onChange={changeConfidence}>
+              <select 
+              id="confidence"
+              name="confidence" 
+              value={inputFields.confidence} 
+              onChange={getInput}>
                 <option value={1}>1</option>
                 <option value={2}>2</option>
                 <option value={3}>3</option>
